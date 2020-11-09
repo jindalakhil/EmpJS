6a8eef5ca67fafff678ec7bb411d6f1d78957147
jindalakhil <akhiljindal222@gmail.com>
2020-11-10 00:38:25 +0530

    [Akhil]Added:using regex on name and applying it in try and catch block

--- empwage.js
+++ empwage.js
@@ -1,39 +1,49 @@
 class EmployeePayrollData{
-    //property
-    id;
-    salary;
-    gender;
-    startDate;
+    //Property
+    id
+    salary
+    gender
+    startDate
 
-    //constructor
+    //Constructor
     constructor(...params){
-        this.id=params[0];
-        this.name=params[1];
-        this.salary=params[2];
-        this.gender=params[3];
-        this.startDate=params[4];
+        this.id = params[0]
+        this.name = params[1]
+        this.salary = params[2]
+        this.gender = params[3]
+        this.startDate = params[4]
     }
 
-    //getter and setter method
-    get name() {return this._name;}
+    //Getter and Setter Methods
+    get name(){
+        return this._name
+    }
     set name(name){
-        
-        this._name=name;
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$')
+        if(nameRegex.test(name)){
+            this._name = name
+        }
+        else throw "Name is Incorrect!"
     }
 
-    //method
+    //Method
     toString(){
-        const options ={year:'numeric', month:'long',day:'numeric'};
-        const empDate=this.startDate===undefined?"undefined":
-        this.startDate.toLocaleDateString("en-US",options);
-        return "id="+this.id+", name="+this.name+", salary="+this.salary+", gender="+this.gender+", start date="+this.startDate;
+        const options = { year : 'numeric', month : 'long', day : 'numeric'}
+        const empDate = this.startDate == undefined ? "undefined" : this.startDate.toLocaleDateString("en-US", options)
+        return "id = " + this.id + ", name = " + this.name + ", salary = " + this.salary + ", gender = " + this.gender + ", startDate = " + empDate
     }
 }
 
-let employeePayrollData = new EmployeePayrollData(1,"Mark",30000);
-console.log(employeePayrollData.toString());
-employeePayrollData.id=0;
-employeePayrollData.name="Jeff";
-console.log(employeePayrollData.toString());
-let employeePayrollData1 = new EmployeePayrollData(1,"Terrisa",30000,"F",new Date());
-console.log(employeePayrollData1.toString());
+let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000)
+console.log(employeePayrollData.toString())
+employeePayrollData.id = 0
+try{
+    employeePayrollData.name = "je"
+    console.log(employeePayrollData.toString())
+}
+catch(e){
+    console.error(e)
+}
+
+let newEmployeePayrollData = new EmployeePayrollData(1, "Terissa", 30000, "F", new Date())
+console.log(newEmployeePayrollData.toString()) 
