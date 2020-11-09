98a230a7f33fe31ba5a7f94e3bfe9f25c21e5a13
jindalakhil <akhiljindal222@gmail.com>
2020-11-10 00:10:28 +0530

    [Akhil]Added: using arrow function computing part time, full time and no wage days

--- empwage.js
+++ empwage.js
@@ -105,4 +105,25 @@
         
         //UC8 Map Functions
         console.log("UC8A -Emp Wage Map totalHrs: "+Array.from(empDailyWageMap.values()).reduce(totalWages,0));
+
+        //UC9 Arrrow function
+        const findTotal = (totalVal,dailyVal)=>{
+            return totalVal+dailyVal;
+        }
+        let totalHours=Array.from(empDailyHrsMap.values())
+                        .filter(dailyHours => dailyHours>0)
+                        .reduce(findTotal,0);
+        let totalSalary = empDailyWageArr.filter(dailyWage=>dailyWage>0).reduce(findTotal,0);
+        console.log("UC9A - Emp wage with Arrow: "+" Total hours: "+totalHours+" Total Wages: "+totalSalary);
+        let nonWorkingDays = new Array();
+        let partWorkingDays = new Array();
+        let fullWorkingDays = new Array();
+        empDailyHrsMap.forEach((value,key)=>{
+            if(value == 8) fullWorkingDays.push(key);
+            else if(value == 4) partWorkingDays.push(key);
+            else nonWorkingDays.push(key);
+        });
+        console.log("Full Working Days: "+fullWorkingDays);
+        console.log("Part Working Days: "+partWorkingDays);
+        console.log("Non Working Days: "+nonWorkingDays);
     }
