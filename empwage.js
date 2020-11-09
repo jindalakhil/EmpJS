f3ceda057c958266874d6c15ea5aa2b9d79532d3
jindalakhil <akhiljindal222@gmail.com>
2020-11-10 00:35:58 +0530

    [Akhil]Added: employee payrol data class created and consoling id,name, salary

--- empwage.js
+++ empwage.js
@@ -1,75 +1,33 @@
-//UC11
-{
-    const IS_PART_TIME = 1;
-    const IS_Full_TIME = 2;
-    const PART_TIME_HOURS = 4;
-    const FULL_TIME_HOURS = 8;
-    const WAGE_PER_HOUR=20;
-    const NUM_OF_WORKING_DAYS=20;
-    const MAX_HRS_IN_MONTH=160;
-    function getEmpHrs(empCheck){
-        switch(empCheck){
-            case IS_PART_TIME:
-                return PART_TIME_HOURS;
-                break;
+class EmployeePayrollData{
+    //property
+    id;
+    salary;
+    gender;
+    startDate;
+
+    //constructor
+    constructor(id,name,salary){
+        this.id=id;
+        this.name=name;
+        this.salary=salary;
+    }
+ 
+
+    //getter and setter method
+    get name() {return this._name;}
+    set name(name){
         
-            case IS_Full_TIME:
-                return FULL_TIME_HOURS;
-                break;
-            default:
-                return 0;
-            }
-        }
+        this._name=name;
+    }
 
-        function calcDailyWage(empHrs){
-            return empHrs*WAGE_PER_HOUR;
-        }
-        let totalEmpHrs=0;
-        let totalWorkingDays=0;
-        let empDailyHrsAndWageArr = new Array();
-        while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS){
-            totalWorkingDays++;
-            let empCheck = Math.floor(Math.random() * 10)%3;
-            empHrs=getEmpHrs(empCheck);
-            totalEmpHrs+=empHrs;
-            empDailyHrsAndWageArr.push(
-                {
-                    dayNum:totalWorkingDays,
-                    dailyHours:empHrs,
-                    dailyWage:calcDailyWage(empHrs),
-                    toString(){
-                        return '\nDay'+ this.dayNum+ ' => Working Hours is '+this.dailyHours+' And Wage Earned= '+ this.dailyWage
-                    },
-                }
-            );
-        }
-        console.log("UC10 Showing daily wage and daily hours " + empDailyHrsAndWageArr);
-
-        //UC10A - UC11D Using object functions along with arrow functions
-
-        let totalWages = empDailyHrsAndWageArr
-                        .filter(dailyHrsAndWage => dailyHrsAndWage.dailyWage > 0)
-                        .reduce((totalWage,dailyHrsAndWage)=>totalWage+=dailyHrsAndWage.dailyWage,0);
-
-        let totalHours = empDailyHrsAndWageArr
-                        .filter(dailyHrsAndWage => dailyHrsAndWage.dailyWage > 0)
-                        .reduce((totalHours,dailyHrsAndWage)=>totalHours+=dailyHrsAndWage.dailyHours,0);
-
-        console.log("UC11A total hours: "+totalHours+ " Total Wages: "+totalWages);
-
-        process.stdout.write("UC11B Logging Full Work")
-        empDailyHrsAndWageArr.filter(dailyHrsAndWage=>dailyHrsAndWage.dailyHours == 8)
-                            .forEach(dailyHrsAndWage => process.stdout.write(dailyHrsAndWage.toString()));
-        console.log("")
-        let partWorkingDayStrArr = empDailyHrsAndWageArr
-                                   .filter(dailyHrsAndWage=>dailyHrsAndWage.dailyHours ==4)
-                                   .map(dailyHrsAndWage=>dailyHrsAndWage.toString());
-        console.log("UC11C PartWorkingDayStrings: "+partWorkingDayStrArr);   
-        
-        let nonWorkingDayNums    =empDailyHrsAndWageArr
-                                .filter(dailyHrsAndWage=>dailyHrsAndWage.dailyHours ==0)
-                                .map(dailyHrsAndWage=>dailyHrsAndWage.toString());
-
-        console.log("UC11D NonWorkingDayNums: "+nonWorkingDayNums);                        
-
+    //method
+    toString(){
+        return "id="+this.id+", name="+this.name+", salary="+this.salary;
+    }
 }
+
+let employeePayrollData = new EmployeePayrollData(1,"Mark",30000);
+console.log(employeePayrollData.toString());
+employeePayrollData.id=0;
+employeePayrollData.name="Jeff";
+console.log(employeePayrollData.toString());
